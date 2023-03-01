707d4196bcdb5dc20cacfd7f6e35617709257809
tutorcs <tutorcs@163.com>
2023-03-01 20:22:42 +0800

    WeChat cstutorcs comp1110 exam 2020s1

---
+++ src/comp1110/exam/Q1Factors.java
@@ -0,0 +1,28 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package comp1110.exam;
+
+/**
+ * COMP1110 Exam, Question 1.1
+ */
+public class Q1Factors {
+    /**
+     * This function takes a positive integer, n, and returns an array
+     * of ints containing all prime factors of that integer in ascending order.
+     * For example:
+     * factors(6) returns {2, 3}
+     * factors{7} returns {7}
+     * factors{24} returns {2, 2, 2, 3}
+     * <p>
+     * If n is less than 2, an empty array is returned.
+     *
+     * @param n the number to factor
+     * @return an array containing all prime factors of the number
+     */
+    public static int[] factors(int n) {
+        // FIXME complete this method
+        return null;
+    }
+}
